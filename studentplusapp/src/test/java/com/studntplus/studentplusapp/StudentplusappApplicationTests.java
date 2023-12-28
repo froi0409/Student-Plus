80e3c5e64b8f7f9e5aa729e3e7dbbaf8fc4caaa9
froi0409 <ferrubocana@gmail.com>
2023-12-27 18:31:12 -0600

    feat: Successfully initialized project

---
+++ studentplusapp/src/test/java/com/studntplus/studentplusapp/StudentplusappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studntplus.studentplusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplusappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
